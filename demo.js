2e9e0a7fad1c815574945e26b221bd2a3aea7b04
Hossam Ahmed <kabtinhosa@yahoo.com>
2024-06-17 02:02:03 +0300

    Add_Code

--- demo.js
+++ demo.js
@@ -3,8 +3,10 @@ let txtEGP = document.getElementById('txtEGP');
 let txtUSD = document.getElementById('txtUSD');
 txtUSD.onkeyup =  function(){
 txtEGP.value = txtUSD.value * 47;
+console.log("EGP : "+txtEGP.value);
 }
 txtEGP.onkeyup =  function(){
   txtUSD.value = txtEGP.value * 47;
+  console.log("USD : "+txtUSD.value);
   }
   
