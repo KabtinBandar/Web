aff6baff8c2a214c32894831f704edee6ae031a8
Hossam Ahmed <kabtinhosa@yahoo.com>
2024-06-18 02:10:30 +0300

    Add_Code

--- demo.js
+++ demo.js
@@ -222,3 +222,17 @@ function searchData(value) {
 
   document.getElementById("tbody").innerHTML = table;
 }
+// // server.mjs
+// import { createServer } from 'node:http';
+
+// const server = createServer((req, res) => {
+//   res.writeHead(200, { 'Content-Type': 'text/plain' });
+//   res.end('Hello World!\n');
+// });
+
+// // starts a simple http server locally on port 3000
+// server.listen(3000, '127.0.0.1', () => {
+//   console.log('Listening on 127.0.0.1:3000');
+// });
+
+// // run with `node server.mjs`
