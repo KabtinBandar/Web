603afd31b14ccedb4a809e55e2ed57c28d6ef2b0
Hossam Ahmed <kabtinhosa@yahoo.com>
2024-06-17 19:29:48 +0300

    Add_CODE

--- demo.js
+++ demo.js
@@ -1,25 +1,73 @@
-let title = document.getElementById('title');
-let price = document.getElementById('price');
-let taxes = document.getElementById('taxes');
-let ads = document.getElementById('ads');
-let discount = document.getElementById('discount');
-let count = document.getElementById('count');
-let total = document.getElementById('total');
-let catgory = document.getElementById('catgory');
-let btnSumbit = document.getElementById('submit');
-
+let title = document.getElementById("title");
+let price = document.getElementById("price");
+let taxes = document.getElementById("taxes");
+let ads = document.getElementById("ads");
+let discount = document.getElementById("discount");
+let count = document.getElementById("count");
+let total = document.getElementById("total");
+let catgory = document.getElementById("catgory");
+let btnSumbit = document.getElementById("submit");
 
 //get total
-function get_total(){
-if (price.value != ''){
-let result = (+price.value + +taxes.value + +ads.value)
-- +discount.value;
-total.innerHTML = result;
-total.style.background = '#040';
-}else{
-    total.innerHTML = '';
-    total.style.background = '#a00d02';
+function get_total() {
+  if (price.value != "") {
+    let result = +price.value + +taxes.value + +ads.value - +discount.value;
+    total.innerHTML = result;
+    total.style.background = "#040";
+  } else {
+    total.innerHTML = "";
+    total.style.background = "#a00d02";
+  }
+}
+let DatePro;
+if (localStorage.Product != null) {
+  DatePro = JSON.parse(localStorage.Product);
+} else {
+  DatePro = [];
+}
 
+btnSumbit.onclick = function() {
+  let newPro = {
+    title: title.value,
+    price: price.value,
+    taxes: taxes.value,
+    ads: ads.value,
+    discount: discount.value,
+    total: total.innerHTML,
+    count: count.value,
+    catgory: catgory.value
+  };
+  DatePro.push(newPro);
+  localStorage.setItem("Product", JSON.stringify(DatePro));
+  ClrearData();
+  showData();
+};
+
+function ClrearData() {
+  title.value = "";
+  price.value = "";
+  taxes.value = "";
+  ads.value = "";
+  discount.value = "";
+  count.value = "";
+  catgory.value = "";
+  total.innerHTML = "";
 }
+
+function showData() {
+  let table = "";
+  for (let i = 0; i < DatePro.length; i++) {
+    table +=
+      '<tr><td>'+i+'</td><td>'+DatePro[i].title+'</td><td>'+DatePro[i].price+'</td><td>'+DatePro[i].taxes+'<td>'+DatePro[i].ads+'</td><td>'+DatePro[i].discount+'</td><td>'+DatePro[i].total+'</td><td>'+DatePro[i].catgory+'</td><td><button id="update">update</button></td><td><button onclick = deleteData('+i+') id="delete">delete</button></td></tr>';
+  }
+  document.getElementById("tbody").innerHTML = table;
 }
+showData();
+
+
+function deleteData(i){
+DatePro.splice(i,1);
+localStorage.Product = JSON.stringify(DatePro);
+showData();
+};
 
